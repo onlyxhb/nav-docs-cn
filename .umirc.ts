7a88a3257d7c0f4225e319bbe56a7d36912409b9
徐辉波 <onlyxhb@qq.com>
2022-09-22 12:52:29 +0800

    feat: vite版本的文档

--- .umirc.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'dumi'
-
-export default defineConfig({
-  title: '官方文档',
-  mode: 'site',
-  base: '/nav-docs-cn/',
-  publicPath: '/nav-docs-cn/'
-  // more config: https://d.umijs.org/config
-})
---
+++ .vitepress/config.ts
@@ -0,0 +1,189 @@
+import { defineConfig } from 'vitepress'
+
+const ogDescription = 'Next Generation Frontend Tooling'
+const ogImage = 'https://vitejs.dev/og-image.png'
+const ogTitle = 'Vite'
+const ogUrl = 'https://vitejs.dev'
+
+export default defineConfig({
+  base: '/nav-docs-cn/',
+  outDir: './dist',
+  title: 'Vite 官方中文文档',
+  description: '下一代前端工具链',
+  lang: 'zh-CN',
+
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:title', content: ogTitle }],
+    ['meta', { property: 'og:image', content: ogImage }],
+    ['meta', { property: 'og:url', content: ogUrl }],
+    ['meta', { property: 'og:description', content: ogDescription }],
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:site', content: '@vite_js' }],
+    ['meta', { name: 'theme-color', content: '#646cff' }]
+  ],
+
+  vue: {
+    reactivityTransform: true
+  },
+
+  themeConfig: {
+    logo: '/logo.svg',
+
+    editLink: {
+      text: '为此页提供修改建议',
+      pattern: 'https://github.com/vitejs/docs-cn/edit/main/:path'
+    },
+
+    algolia: {
+      appId: '7H67QR5P0A',
+      apiKey: 'deaab78bcdfe96b599497d25acc6460e',
+      indexName: 'vitejs',
+      searchParameters: {
+        facetFilters: ['tags:cn']
+      }
+    },
+
+    carbonAds: {
+      code: 'CEBIEK3N',
+      placement: 'vitejsdev'
+    },
+
+    footer: {
+      copyright: '本中文文档内容版权为 Vite 官方中文翻译团队所有，保留所有权利。'
+    },
+
+    nav: [
+      { text: '指引', link: '/guide/', activeMatch: '/guide/' },
+      { text: '配置', link: '/config/', activeMatch: '/config/' },
+      { text: '插件', link: '/plugins/', activeMatch: '/plugins/' }
+    ],
+
+    sidebar: {
+      '/guide/': [
+        {
+          text: '指引',
+          items: [
+            {
+              text: '为什么选 Vite',
+              link: '/guide/why'
+            },
+            {
+              text: '开始',
+              link: '/guide/'
+            },
+            {
+              text: '功能',
+              link: '/guide/features'
+            },
+            {
+              text: '使用插件',
+              link: '/guide/using-plugins'
+            },
+            {
+              text: '依赖预构建',
+              link: '/guide/dep-pre-bundling'
+            },
+            {
+              text: '静态资源处理',
+              link: '/guide/assets'
+            },
+            {
+              text: '构建生产版本',
+              link: '/guide/build'
+            },
+            {
+              text: '部署静态站点',
+              link: '/guide/static-deploy'
+            },
+            {
+              text: '环境变量与模式',
+              link: '/guide/env-and-mode'
+            },
+            {
+              text: '服务端渲染（SSR）',
+              link: '/guide/ssr'
+            },
+            {
+              text: '后端集成',
+              link: '/guide/backend-integration'
+            },
+            {
+              text: '比较',
+              link: '/guide/comparisons'
+            },
+            {
+              text: '故障排除',
+              link: '/guide/troubleshooting'
+            },
+            {
+              text: '从 v2 迁移',
+              link: '/guide/migration'
+            }
+          ]
+        },
+        {
+          text: 'API',
+          items: [
+            {
+              text: '插件 API',
+              link: '/guide/api-plugin'
+            },
+            {
+              text: 'HMR API',
+              link: '/guide/api-hmr'
+            },
+            {
+              text: 'JavaScript API',
+              link: '/guide/api-javascript'
+            },
+            {
+              text: '配置参考',
+              link: '/config/'
+            }
+          ]
+        }
+      ],
+      '/config/': [
+        {
+          text: '配置',
+          items: [
+            {
+              text: '配置 Vite',
+              link: '/config/'
+            },
+            {
+              text: '共享选项',
+              link: '/config/shared-options'
+            },
+            {
+              text: '服务器选项',
+              link: '/config/server-options'
+            },
+            {
+              text: '构建选项',
+              link: '/config/build-options'
+            },
+            {
+              text: '预览选项',
+              link: '/config/preview-options'
+            },
+            {
+              text: '依赖优化选项',
+              link: '/config/dep-optimization-options'
+            },
+            {
+              text: 'SSR 选项',
+              link: '/config/ssr-options'
+            },
+            {
+              text: 'Worker 选项',
+              link: '/config/worker-options'
+            }
+          ]
+        }
+      ]
+    }
+  }
+})
